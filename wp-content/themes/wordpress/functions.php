0dee710498533d8448ed69ccd0b46cdce1e54414
Henrique Da Silva Marques <henrique.marques2@USCMAC139.local>
2020-02-17 15:10:19 -0300

    Include library files to functions.php

--- wp-content/themes/wordpress/functions.php
+++ wp-content/themes/wordpress/functions.php
@@ -20,10 +20,38 @@ function dd($var){
 	die;
 }
 
+include __DIR__ . '/library/wp_wrapper/bootstrap.php';
+
+require __DIR__ . '/library/images.php';
+require __DIR__ . '/library/templates.php';
+require __DIR__ . '/library/pagebuilder.php';
+require __DIR__ . '/library/post_types.php';
+require __DIR__ . '/library/metaboxes.php';
+require __DIR__ . '/library/taxonomies.php';
+require __DIR__ . '/library/api.php';
+
 require_once 'JAMStack/JAMStack.php';
 require_once 'JAMStack/Utils.php';
 require_once 'JAMStack/interfaces/IContentGenerator.php';
 require_once 'JAMStack/generators/ContentGenerator.php';
 require_once 'JAMStack/generators/ListGenerator.php';
 
+$pb_scripts = [];
 $jamstack = new JAMStack();
+
+/*
+* * * Globally set page builder component's data
+*/
+function localize_scripts($object_name, $hash, $l10n){
+    global $pb_scripts;
+
+    foreach ( (array) $l10n as $key => $value ) {
+        if ( ! is_scalar( $value ) ) {
+            continue;
+        }
+
+        $l10n[ $key ] = html_entity_decode( (string) $value, ENT_QUOTES, 'UTF-8' );
+    }
+
+    $pb_scripts[$object_name][$hash] = $l10n;
+}
