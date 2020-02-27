11606778b1e94e727bf493260bd0b7bfd336f44b
Henrique Marques <henrique1_3@hotmail.com>
2020-02-27 12:30:30 -0300

    Archive endpoint

--- wp-content/themes/wordpress/library/api.php
+++ wp-content/themes/wordpress/library/api.php
@@ -1,15 +1,28 @@
 <?php
 add_action('rest_api_init', function () {
-    register_rest_route('dna/v1', '/newsletter', array(
+    register_rest_route('smart/v1', '/newsletter', array(
         'methods' => 'POST',
         'callback' => 'smart_newsletter',
     ));
+
+    register_rest_route('smart/v1', '/archive', array(
+        'methods' => 'GET',
+        'callback' => 'smart_archive',
+    ));
 });
 
+function smart_add_custom_endpoint( $allowed_endpoints ) {
+    if ( ! isset( $allowed_endpoints[ 'smart/v1' ] ) || ! in_array( 'archive', $allowed_endpoints[ 'smart/v1' ] ) ) {
+        $allowed_endpoints[ 'smart/v1' ][] = 'archive';
+    }
+    return $allowed_endpoints;
+}
+add_filter( 'wp_rest_cache/allowed_endpoints', 'smart_add_custom_endpoint', 10, 1);
+
 function smart_newsletter(WP_REST_Request $request) {
-    $name = $param = $request->get_param( 'name' );
-    $email = $param = $request->get_param( 'email' );
-    $whatsapp = $param = $request->get_param( 'whatsapp' );
+    $name = $request->get_param( 'name' );
+    $email = $request->get_param( 'email' );
+    $whatsapp = $request->get_param( 'whatsapp' );
     
     if(!empty($name) && !empty($email) && !empty($whatsapp) ) {
         $lead = wp_insert_post([
@@ -27,5 +40,62 @@ function smart_newsletter(WP_REST_Request $request) {
     }
 
     return [ 'post' => $lead ];
+}
+
+function smart_archive( WP_REST_Request $request ) {
+    $term_slug = $request->get_param('term');
+
+    $term = get_terms([ 
+        'get'                    => 'all',
+        'number'                 => 1,
+        'update_term_meta_cache' => false,
+        'orderby'                => 'none',
+        'suppress_filter'        => true,
+        'slug' => $term_slug
+    ]);
+
+    if( count($term) > 0 ) {
+        $term = $term[0];
+    }
+
+    $relations = [];
+
+    $terms_query = new WP_Query( [
+        'ppp' => '40',
+        'post_status' => 'publish',
+        'tax_query' => array(
+            array(
+                'taxonomy' => $term->taxonomy,
+                'field'    => 'term_id',
+                'terms'    => $term->term_id,
+            ),
+        ),
+    ] );
+
+    while($terms_query->have_posts()) {
+        $terms_query->the_post();
+        global $post;
+        
+        $class_name = str_replace(' ', '', ucwords(str_replace('-', ' ', $post->post_type)));
+        if ( !class_exists($class_name) ) {
+            $class_name = 'ContentGenerator'; 
+        }
+
+        $generator = new $class_name($post, []);
+        $relations[] = $generator->prepare_post( $post, [ 'content', 'tags', 'meta' ] );
+    }
+
+    \wp_reset_query();
+
+    return [
+        'ID' => $term->term_id,
+        'title' => $term->name,
+        'excerpt' => $term->description,
+        'slug' => $term->slug,
+        'order' => $term->order,
+        'parent' => $term->parent,
+        'type' => $term->taxonomy,
+        'relations' => $relations,
+    ];
 }
   
