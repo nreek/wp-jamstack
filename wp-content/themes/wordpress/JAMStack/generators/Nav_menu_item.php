371e07ca5c9f51b4e7889fd9733c3731c80737f1
Henrique Marques <henrique1_3@hotmail.com>
2020-03-10 11:37:01 -0300

    Fixing nav menu itens structure and IDs

--- wp-content/themes/wordpress/JAMStack/generators/Nav_menu_item.php
+++ wp-content/themes/wordpress/JAMStack/generators/Nav_menu_item.php
@@ -9,7 +9,6 @@ class Nav_menu_item extends ContentGenerator {
 
         foreach ($nav_menus as $menu){
             $nav_items = wp_get_nav_menu_items($menu);
-
             $menus[$menu->slug] = [
                 'ID' => $menu->term_id,
                 'name' => $menu->name,
@@ -30,11 +29,13 @@ class Nav_menu_item extends ContentGenerator {
         foreach( $request_items as $request_id => $type ){
             $url = isset($_REQUEST['menu-item-url'][$request_id]) ? $_REQUEST['menu-item-url'][$request_id] : get_the_permalink($_REQUEST['menu-item-object-id'][$request_id]);
             
+
             $items[] = [
-                'ID' => $_REQUEST['menu-item-object-id'][$request_id],
+                'ID' => $request_id,
                 'title' => $_REQUEST['menu-item-title'][$request_id],
-                'permalink' => $url,
+                'permalink' => replace_home_url($url),
                 'type' => $type,
+                'slug' =>  sanitize_title($_REQUEST['menu-item-title'][$request_id]),
                 'order' => $_REQUEST['menu-item-position'][$request_id],
                 'parent' => $_REQUEST['menu-item-parent-id'][$request_id],
             ];
@@ -46,24 +47,21 @@ class Nav_menu_item extends ContentGenerator {
     function prepare_menu($menu, $nav_items){
         $items = [];
 
+        
         foreach($nav_items as $item){
             $items[] = [
                 'ID' => $item->ID,
                 'title' => $item->title,
-                'permalink' => $item->url,
+                'permalink' => replace_home_url($item->url),
                 'object' => $item->object,
                 'type' => $item->type,
+                'slug' => $item->post_name,
                 'order' => $item->menu_order,
                 'parent' => $item->menu_item_parent
             ];
         }
-        
-        return [
-            'ID' => $menu->term_id,
-            'title' => $menu->name,
-            'slug' => $menu->slug,
-            'items' => $items,
-        ];
+
+        return $items;
     }
 
     function generate_json(){
