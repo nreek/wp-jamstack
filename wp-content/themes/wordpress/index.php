85a5a259ec88bfcf7ca8405f491725c6c1c3f144
Henrique Marques <henrique1_3@hotmail.com>
2020-03-11 17:32:41 -0300

    Added logged in endpoint

--- wp-content/themes/wordpress/index.php
+++ wp-content/themes/wordpress/index.php
@@ -1,1 +1,11 @@
+<?php 
+if( isset($_GET['is_loggedin']) ) {
+    echo wp_json_encode( [
+        'is_loggedin'   => is_user_logged_in(),
+        'admin_url'     => get_admin_url()
+    ] );
+
+    die;
+}
+
 header('Location: '.'http://localhost:3000'.$_SERVER['REQUEST_URI']);
